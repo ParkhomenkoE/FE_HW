3e03995dd8563adf0387150e573c150863178552
ParkhomenkoE <114993005+ParkhomenkoE@users.noreply.github.com>
2022-10-16 18:19:23 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,5 +1,31 @@
+package lesson.classwork;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // локальная переменная
+        /*byte
+                short
+                        int
+                                long*/
+        //byte byteValue = 120;
+        byte value = 127;
+        byte value2 = -128;
+
+        short shortValue = 32535;
+
+        int intValue = 2_000_000_000;
+
+        long peopleCountInEarth = 800L;
+
+        float floatValue = 10.3f;
+
+        double doubleValue = 100.4;
+
+        String message = "Message from us";
+
+        boolean booleanValue = false;
+
+
+
     }
 }
