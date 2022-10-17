f3bff83c1de9aac041572dd94942e95c8a8c5cae
ParkhomenkoE <114993005+ParkhomenkoE@users.noreply.github.com>
2022-10-17 09:14:48 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-package lesson.classwork;
+package lesson.homework;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,23 +9,73 @@ public class Main {
                                 long*/
         //byte byteValue = 120;
         byte value = 127;
-        byte value2 = -128;
-
         short shortValue = 32535;
-
         int intValue = 2_000_000_000;
-
         long peopleCountInEarth = 800L;
 
-        float floatValue = 10.3f;
+        /*
+        * 
+        * First level: 
+        */
 
-        double doubleValue = 100.4;
+        /*First level: Задание 1.
+        В методе main инициализировать все целочисленные примитивные типы 
+        и вывести их в консоль в примерном виде "Переменная с типом 
+        <Здесь вы выводите название типа переменной>
+        и названием <Здесь вы выводите название вашей переменной>
+        имеет значение равное <Здесь вы выводите значение вашей переменной>"
+        */
+        System.out.println("Задание 1");
+        //щоб кілька разів не писать одне й те саме зробимо кілька змінних для строчок
+        String message1 = "Переменная с типом ";
+        String message2 = " и названием ";
+        String message3 = " имеет значение равное ";
+        //а так як змінні вже є вище  просто використаємо їх
+        System.out.println(message1+"byte"+message2+"value"+message3);
+        System.out.println(value); //Виводимо значення самої змінної
+        //... аналогічно для кожної
+        System.out.println(message1+"short"+message2+"shortValue"+message3);
+        System.out.println(shortValue);
+        System.out.println(message1+"int"+message2+"intValue"+message3);
+        System.out.println(intValue);
+        System.out.println(message1+"long"+message2+"peopleCountInEarth"+message3);
+        System.out.println(peopleCountInEarth);
 
-        String message = "Message from us";
+        /*Задание 2.
+        Напишите программу, которая выводит на экран сумму двух чисел.
+        54 + 16
+        */
+        System.out.println("Задание 2");
+        System.out.println(54+16); // шоб не об'являть змінну - просто виконуємо операції зразу "на друк"
+        
+        /*Задание 3.
+        Напишите программу, которая выводит на экран результат деления двух чисел.
+        50/3
+        */
+        System.out.println("Задание 3");
+        System.out.println(50/3); // тут непонятно, треба Float чи можна й так - цілочисленне
+        
+        /*Задание 4.
+        Напишите программу, которая выводит на экран значение переменной типа int  
+        в квадрате (n* n). Предварительно объявите эту переменную и задайте ее значение.
+        */
+        System.out.println("Задание 4");
+        int toSqrt = 5; //створюємо змінну
+        System.out.println(toSqrt*toSqrt);// виконуємо оперцію сразу в друк на консоль
 
-        boolean booleanValue = false;
-
-
+        /*
+        Second level: Объявите 10 переменных типа int со значениями 
+        0,1, 2, 3, 4, 5, 6, 7, 8, 9. Объявите ещё одну переменную int
+        и сохраните в неё сумму этих переменных, деленную на 10. Получается, 
+        у вас будет среднее арифметическое этих чисел в одной переменной. 
+        Распечатайте её через println. 
+        */
+        System.out.println("Second level");
+        int zero = 0, one = 1, two = 2, three = 3, four = 4, five = 5, six = 6,
+        seven = 7, eight = 8, nine = 9; 
+        int avge; //об'являємо, але пока нічого не присваюємо
+        avge = (zero+one+two+three+four+five+six+seven+eight+nine)/10; //присваєюмо результат математичної операції
+        System.out.println(avge); // і друкуємо його
 
     }
 }
